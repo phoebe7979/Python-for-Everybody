e249b307a96bf370fef1ba9ddf8dee953110fd1f
phoebe7979 <phoebe7979@gmail.com>
2017-09-25 00:22:16 +0800

    fixed bug

--- ex_3_3_prompt_score.py
+++ ex_3_3_prompt_score.py
@@ -7,14 +7,14 @@ except:
     quit()
 if fscore < 0.6:
     print("F")
-if fscore < 0.7 and fscore >= 0.6:
+elif fscore < 0.7 and fscore >= 0.6:
     print("D")
-if fscore < 0.8 and fscore >= 0.7:
+elif fscore < 0.8 and fscore >= 0.7:
     print("C")
-if fscore < 0.9 and fscore >= 0.8:
+elif fscore < 0.9 and fscore >= 0.8:
     print("B")
-if fscore <= 1 and fscore >= 0.9:
+elif fscore <= 1 and fscore >= 0.9:
     print ("A")
 else:
-    print ("ERROR. Please enter a numeric value between 0.0 and 1.0.")
+    print ("ERROR. Please enter a numeric value between 0.0 and 1.0")
     quit()
