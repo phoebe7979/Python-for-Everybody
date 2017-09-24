453d37e471c9619553928eeac94d03f89d622486
phoebe7979 <phoebe7979@gmail.com>
2017-09-24 23:49:52 +0800

    making the code neater

--- ex_3_1.py
+++ ex_3_1.py
@@ -1,10 +1,10 @@
 # Goal is to set 1x hourly rate when below 40 hrs and 1.5x hourly rate when
 # above 40 hours.
 hrs = input("Enter Hours:")
-h = float(hrs)
 rate = input("Enter Rates:")
 r = float(rate)
+h = float(hrs)
 if h <= 40:
     print(h * r)
-if h > 40:
+else:
     print((40 * r) + ((h - 40) * (1.5 * r)))
