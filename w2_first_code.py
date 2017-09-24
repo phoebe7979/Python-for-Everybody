d18e07dd0bde2b9e0428dad838985198196d5f50
phoebe7979 <phoebe7979@gmail.com>
2017-09-24 21:19:06 +0800

    trying

---
+++ w2_first_code.py
@@ -0,0 +1,4 @@
+print('hello from your first python file')
+x = 1
+x = x+1
+print (x)
---
+++ w4_ex_2_3_calculate_pay.py
@@ -0,0 +1,4 @@
+# Math test: Calculating the pay with 2.75 hourly rate and 35 hours of work
+hrs = input("Enter Hours:")
+rate = input("Enter Rate:")
+print ('Pay:', float(rate) * float(hrs))
