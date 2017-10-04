7adf27fe5c7eb7cd533f742ba430e34370e37ddc
phoebe7979 <phoebe7979@gmail.com>
2017-10-04 22:51:22 +0800

    adjusted after watching the tutorial video

--- ex_8_5/ex_8_5_mail.py
+++ ex_8_5/ex_8_5_mail.py
@@ -3,12 +3,10 @@ fileopen = open(filename)
 count = 0
 for line in fileopen:
     linestrip = line.rstrip()
-    if line.startswith('From:'):
+    words = linestrip.split()
+    if len(words) < 2 or words[0] != 'From':
         continue
-    elif line.startswith('From'):
-        splitline = line.split()
-        print(splitline[1])
+    if words[0] == 'From':
+        print(words[1])
         count = count + 1
-    else:
-        continue
 print('There were', count ,'lines in the file with From as the first word')
