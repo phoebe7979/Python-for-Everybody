3eeb64abee7cfb8e2dcc03072a4766c85b354f2a
phoebe7979 <phoebe7979@gmail.com>
2018-07-08 22:03:27 +0800

    contructed a database after extracting information form teh .txt file and ordered the database by counts.

--- ex_15_2/ex_15_2.py
+++ ex_15_2/ex_15_2.py
@@ -18,10 +18,29 @@ fh = open(fname)
 
 # Selecting the correct lines #
 for line in fh:
-    if not line.startswith('From'):
+    if not line.startswith('From: '):
         continue
     pieces = line.split()
-    print(pieces)
+
+# Identifying the emails and domains #
+    email = pieces[1]
+    org = email.split('@') [1]
+
+# Inserting data into the database #
+    cur.execute('SELECT count FROM Counts WHERE org = ?', (org,))
+    row = cur.fetchone()
+    if row is None:
+        cur.execute('''INSERT INTO Counts (org, count) VALUES (?, 1)''', (org,))
+    else:
+        cur.execute('''UPDATE Counts SET count = count + 1 WHERE org = ?''', (org,))
+
+# Commiting the updates #
+    conn.commit()
+
+# Ordering the counts and printing the results #
+sqlstring = 'SELECT * FROM Counts ORDER BY count DESC'
+for row in cur.execute(sqlstring):
+    print(str(row[0]), row[1])
 
 # Closing the connection #
 conn.close()
